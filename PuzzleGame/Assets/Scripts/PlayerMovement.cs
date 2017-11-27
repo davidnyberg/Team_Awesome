7be9cd11a75134a7cc763a02f5f710cb3f622b3d
Cole Proctor <proctorcole@gmail.com>
2017-11-27 12:34:53 -0700

    Movement and Animation

---
+++ PuzzleGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed; // Changes Player Movement Speed
+
+	private Animator anim;
+
+    private void Start() // First frame of "scene"
+    {
+		anim = GetComponent<Animator>();
+    }
+
+    private void Update() // Updates every "frame"
+    {
+		if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) // Horizontal Movement
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+		}		
+		if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) // Vertical Movement
+		{
+			transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+		}
+		anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
+		anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
+    }
+}
