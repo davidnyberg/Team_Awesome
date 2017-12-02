33e191ab02d7527e04293a8782db090c097fb162
Cole Proctor <proctorcole@gmail.com>
2017-12-02 12:49:35 -0700

    Blend Tree and Animation Fixing

--- PuzzleGame/Assets/Scripts/PlayerMovement.cs
+++ PuzzleGame/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,9 @@ public class PlayerMovement : MonoBehaviour
 
 	private Animator anim;
 
+	private bool playerMoving;
+	private Vector2 lastMove;
+
     private void Start() // First frame of "scene"
     {
 		anim = GetComponent<Animator>();
@@ -16,15 +19,23 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update() // Updates every "frame"
     {
+		playerMoving = false;
 		if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) // Horizontal Movement
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+			playerMoving = true;
+			lastMove = new Vector2 (Input.GetAxisRaw ("Horizontal"), 0f);
 		}		
 		if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) // Vertical Movement
 		{
 			transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+			playerMoving = true;
+			lastMove = new Vector2 (0f, Input.GetAxisRaw ("Vertical"));
 		}
 		anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
 		anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
+		anim.SetBool ("PlayerMoving", playerMoving);
+		anim.SetFloat ("LastMoveX", lastMove.x);
+		anim.SetFloat ("LastMoveY", lastMove.y);
     }
 }
